b0c713ca101059cbb9c1760703e885d1ba88e717
Shiquan Sun <me@imsun.net>
2016-07-02 02:54:51 +0800

    init

---
+++ index.js
@@ -0,0 +1,37 @@
+var Server = require('bittorrent-tracker').Server
+
+var server = new Server({
+	udp: false, // enable udp server? [default=true]
+	http: false, // enable http server? [default=true]
+	ws: true, // enable websocket server? [default=true]
+	stats: true // enable web-based statistics? [default=true]
+})
+
+server.on('error', function (err) {
+	// fatal server error!
+	console.log(err.message)
+})
+
+server.on('warning', function (err) {
+	// client sent bad data. probably not a problem, just a buggy client.
+	console.log(err.message)
+})
+
+server.on('listening', function () {
+	// fired when all requested servers are listening
+	console.log('listening on http port:' + server.http.address().port)
+	console.log('listening on udp port:' + server.udp.address().port)
+})
+
+// start tracker server listening! Use 0 to listen on a random free port.
+server.listen(80, hostname, onlistening)
+
+// listen for individual tracker messages from peers:
+
+server.on('start', function (addr) {
+	console.log('got start message from ' + addr)
+})
+
+server.on('complete', function (addr) {})
+server.on('update', function (addr) {})
+server.on('stop', function (addr) {})
